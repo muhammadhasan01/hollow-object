fdf401480122f3da02feff052d75464a1073c37a
Mardiano <13518039@std.stei.itb.ac.id>
2021-03-22 23:35:03 +0700

    add rotation scale and translation

--- webgl-demo.js
+++ webgl-demo.js
@@ -1,10 +1,150 @@
 var cubeRotation = 0.0;
 
-main();
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
 
 //
 // Start here
 //
+function translate(out, a, v) {
+  let x = v[0],
+    y = v[1],
+    z = v[2];
+  let a00, a01, a02, a03;
+  let a10, a11, a12, a13;
+  let a20, a21, a22, a23;
+  if (a === out) {
+    out[12] = a[0] * x + a[4] * y + a[8] * z + a[12];
+    out[13] = a[1] * x + a[5] * y + a[9] * z + a[13];
+    out[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
+    out[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
+  } else {
+    a00 = a[0];
+    a01 = a[1];
+    a02 = a[2];
+    a03 = a[3];
+    a10 = a[4];
+    a11 = a[5];
+    a12 = a[6];
+    a13 = a[7];
+    a20 = a[8];
+    a21 = a[9];
+    a22 = a[10];
+    a23 = a[11];
+    out[0] = a00;
+    out[1] = a01;
+    out[2] = a02;
+    out[3] = a03;
+    out[4] = a10;
+    out[5] = a11;
+    out[6] = a12;
+    out[7] = a13;
+    out[8] = a20;
+    out[9] = a21;
+    out[10] = a22;
+    out[11] = a23;
+    out[12] = a00 * x + a10 * y + a20 * z + a[12];
+    out[13] = a01 * x + a11 * y + a21 * z + a[13];
+    out[14] = a02 * x + a12 * y + a22 * z + a[14];
+    out[15] = a03 * x + a13 * y + a23 * z + a[15];
+  }
+  return out;
+}
+
+function rotate(out, a, rad, axis) {
+  let x = axis[0],
+    y = axis[1],
+    z = axis[2];
+  let len = Math.hypot(x, y, z);
+  let s, c, t;
+  let a00, a01, a02, a03;
+  let a10, a11, a12, a13;
+  let a20, a21, a22, a23;
+  let b00, b01, b02;
+  let b10, b11, b12;
+  let b20, b21, b22;
+  if (len < glMatrix.EPSILON) {
+    return null;
+  }
+  len = 1 / len;
+  x *= len;
+  y *= len;
+  z *= len;
+  s = Math.sin(rad);
+  c = Math.cos(rad);
+  t = 1 - c;
+  a00 = a[0];
+  a01 = a[1];
+  a02 = a[2];
+  a03 = a[3];
+  a10 = a[4];
+  a11 = a[5];
+  a12 = a[6];
+  a13 = a[7];
+  a20 = a[8];
+  a21 = a[9];
+  a22 = a[10];
+  a23 = a[11];
+  // Construct the elements of the rotation matrix
+  b00 = x * x * t + c;
+  b01 = y * x * t + z * s;
+  b02 = z * x * t - y * s;
+  b10 = x * y * t - z * s;
+  b11 = y * y * t + c;
+  b12 = z * y * t + x * s;
+  b20 = x * z * t + y * s;
+  b21 = y * z * t - x * s;
+  b22 = z * z * t + c;
+  // Perform rotation-specific matrix multiplication
+  out[0] = a00 * b00 + a10 * b01 + a20 * b02;
+  out[1] = a01 * b00 + a11 * b01 + a21 * b02;
+  out[2] = a02 * b00 + a12 * b01 + a22 * b02;
+  out[3] = a03 * b00 + a13 * b01 + a23 * b02;
+  out[4] = a00 * b10 + a10 * b11 + a20 * b12;
+  out[5] = a01 * b10 + a11 * b11 + a21 * b12;
+  out[6] = a02 * b10 + a12 * b11 + a22 * b12;
+  out[7] = a03 * b10 + a13 * b11 + a23 * b12;
+  out[8] = a00 * b20 + a10 * b21 + a20 * b22;
+  out[9] = a01 * b20 + a11 * b21 + a21 * b22;
+  out[10] = a02 * b20 + a12 * b21 + a22 * b22;
+  out[11] = a03 * b20 + a13 * b21 + a23 * b22;
+  if (a !== out) {
+    // If the source and destination differ, copy the unchanged last row
+    out[12] = a[12];
+    out[13] = a[13];
+    out[14] = a[14];
+    out[15] = a[15];
+  }
+  return out;
+}
+
+function scale(out, a, v) {
+  let x = v[0],
+    y = v[1],
+    z = v[2];
+  out[0] = a[0] * x;
+  out[1] = a[1] * x;
+  out[2] = a[2] * x;
+  out[3] = a[3] * x;
+  out[4] = a[4] * y;
+  out[5] = a[5] * y;
+  out[6] = a[6] * y;
+  out[7] = a[7] * y;
+  out[8] = a[8] * z;
+  out[9] = a[9] * z;
+  out[10] = a[10] * z;
+  out[11] = a[11] * z;
+  out[12] = a[12];
+  out[13] = a[13];
+  out[14] = a[14];
+  out[15] = a[15];
+  return out;
+}
+
 function main() {
   const canvas = document.querySelector('#glcanvas');
   const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
@@ -29,6 +169,8 @@ function main() {
       vColor = aVertexColor;
     }
   `;
+
+
 
   // Fragment shader program
 
@@ -97,49 +239,31 @@ function initBuffers(gl) {
 
   // Now create an array of positions for the cube.
 
-  const positions = [
+  const positions2 = [
     // Front face
-    -1.0, -1.0,  1.0,
-     1.0, -1.0,  1.0,
+    -1.0, -1.0,  0.0,
+     1.0, -1.0,  0.0,
      1.0,  1.0,  1.0,
     -1.0,  1.0,  1.0,
 
     // Back face
-    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 0.0,
     -1.0,  1.0, -1.0,
      1.0,  1.0, -1.0,
-     1.0, -1.0, -1.0,
+     1.0, -1.0, 0.0,
 
     // Top face
     -1.0,  1.0, -1.0,
     -1.0,  1.0,  1.0,
      1.0,  1.0,  1.0,
      1.0,  1.0, -1.0,
-
-    // Bottom face
-    -1.0, -1.0, -1.0,
-     1.0, -1.0, -1.0,
-     1.0, -1.0,  1.0,
-    -1.0, -1.0,  1.0,
-
-    // Right face
-     1.0, -1.0, -1.0,
-     1.0,  1.0, -1.0,
-     1.0,  1.0,  1.0,
-     1.0, -1.0,  1.0,
-
-    // Left face
-    -1.0, -1.0, -1.0,
-    -1.0, -1.0,  1.0,
-    -1.0,  1.0,  1.0,
-    -1.0,  1.0, -1.0,
   ];
 
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(  positions2), gl.STATIC_DRAW);
 
   // Now set up the colors for the faces. We'll use solid colors
   // for each face.
@@ -148,9 +272,7 @@ function initBuffers(gl) {
     [1.0,  1.0,  1.0,  1.0],    // Front face: white
     [1.0,  0.0,  0.0,  1.0],    // Back face: red
     [0.0,  1.0,  0.0,  1.0],    // Top face: green
-    [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
-    [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-    [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+
   ];
 
   // Convert the array of colors into a table for all the vertices.
@@ -239,18 +361,41 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 
   // Now move the drawing position a bit to where we want to
   // start drawing the square.
+  var x = -document.getElementById("x").value / 100;
+  var y = -document.getElementById("y").value / 100;
+  var z = -document.getElementById("z").value / 100;
+  var angleX = document.getElementById("angleX").value / 100;
+  var angleY = document.getElementById("angleY").value / 100;
+  var angleZ = document.getElementById("angleZ").value / 100;
+  var scales = document.getElementById("scale").value;
 
-  mat4.translate(modelViewMatrix,     // destination matrix
-                 modelViewMatrix,     // matrix to translate
-                 [-0.0, -0.0, -5.5]);  // amount to translate
-  mat4.rotate(modelViewMatrix,  // destination matrix
-              modelViewMatrix,  // matrix to rotate
-              cubeRotation,     // amount to rotate in radians
-              [0, 0, 1]);       // axis to rotate around (Z)
-  mat4.rotate(modelViewMatrix,  // destination matrix
-              modelViewMatrix,  // matrix to rotate
-              cubeRotation * .7,// amount to rotate in radians
-              [0, 1, 0]);       // axis to rotate around (X)
+//Translation
+  translate(modelViewMatrix,    // destination matrix
+            modelViewMatrix,    // matrix to translate
+            [x, y, z]);         // amount to translate
+
+//Rotation on X axis
+  rotate(modelViewMatrix,   // destination matrix
+          modelViewMatrix,    // matrix to rotate
+          angleX,             // amount to rotate in radians
+          [0, 1, 0]);         // axis to rotate around (X)
+        
+//Rotation on Y axis
+  rotate(modelViewMatrix,   // destination matrix
+        modelViewMatrix,    // matrix to rotate
+        angleY,             // amount to rotate in radians
+        [1, 0, 0]);         // axis to rotate around (Y)
+
+//Rotation on Z axis
+  rotate(modelViewMatrix,   // destination matrix
+        modelViewMatrix,    // matrix to rotate
+        angleZ,             // amount to rotate in radians
+        [0, 0, 1]);         // axis to rotate around (Z)
+
+//Scale
+scale(modelViewMatrix,        // destination matrix
+  modelViewMatrix,            // matrix to translate
+  [scales, scales, scales]);    // amount to translate
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute
@@ -311,7 +456,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
       modelViewMatrix);
 
   {
-    const vertexCount = 36;
+    const vertexCount = 20;
     const type = gl.UNSIGNED_SHORT;
     const offset = 0;
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
@@ -371,3 +516,5 @@ function loadShader(gl, type, source) {
 
   return shader;
 }
+
+main();
