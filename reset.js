4db6581c0e6b960e13d673b7de3c85d7d5afa293
Muhamamad Hasan <13518012@std.stei.itb.ac.id>
2021-03-23 22:08:10 +0700

    feat: add reset

---
+++ reset.js
@@ -0,0 +1,12 @@
+function handleReset() {
+    document.getElementById('perspectiveOption').value = 'perspective';
+    document.getElementById('cameraAngle').value = 50;
+    document.getElementById('cameraZoom').value = 50;
+    document.getElementById("x").value = 10;
+    document.getElementById("y").value = 0;
+    document.getElementById("z").value = 0;
+    document.getElementById("angleX").value = 200;
+    document.getElementById("angleY").value = 320;
+    document.getElementById("angleZ").value = 0;
+    document.getElementById("scale").value = 1;
+}
